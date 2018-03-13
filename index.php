8a4f50f9c139d994b1682db695e840f31f95029f
Vlad Oseyev <vladoseyev3@gmail.com>
2018-03-13 14:30:11 -0400

    added index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once('vendor/autoload.php');
+session_start();
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    $view = new View();
+    echo $view->render('pages/home.html');
+}
+);
+
+$f3->run();
