28ec4425ce9132d40273bccb9076c2eb499bb667
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-15 19:04:55 -0400

    fixed routing bug

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set("display_errors", 3);
 error_reporting(E_ALL);
+
 
 //require the autoload file
 require_once('vendor/autoload.php');
@@ -11,16 +11,21 @@ session_start();
 //create an instance of the base class
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG', 3);
+
 //define a default route
-$f3->route('GET /', function() {
-    $view = new View();
-    echo $view->render('pages/home.html');
+$f3 -> route('GET /', function() {
+    $template = new Template();
+    echo $template->render('pages/home.html');
 }
 );
 
-$f3->route('GET|POST /information', function(){
+$f3->route('GET|POST /information', function() {
     $template = new Template();
     echo $template->render('pages/information.html');
 });
+
+
 
 $f3->run();
