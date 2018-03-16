41f2836ce8f9a47f22d56a74d87fff4d5b3c83e0
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-16 01:03:20 -0400

    added additional routes

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
 
+//require the autoload file
+require_once('vendor/autoload.php');
+
 ini_set("display_errors", 3);
 error_reporting(E_ALL);
 
 
-//require the autoload file
-require_once('vendor/autoload.php');
+
 session_start();
 
 //create an instance of the base class
 $f3 = Base::instance();
 
 //Set debug level
-$f3->set('DEBUG', 3);
+$f3 -> set('DEBUG', 1);
 
 //define a default route
 $f3 -> route('GET /', function() {
@@ -21,10 +23,41 @@ $f3 -> route('GET /', function() {
 }
 );
 
-$f3->route('GET|POST /information', function() {
+
+$f3 -> route('GET|POST /estimate', function($f3) {
+    if(isset($_POST['submit']))
+    {
+        $location = $_POST['location'];
+        $sqrFt = $_POST['sqrFt'];
+        $errors = $_POST['errors'];
+
+        if(!isset($errors['sqrFt']) && !isset($errors['location']))
+        {
+            $_SESSION['location']=$location;
+            $_SESSION['sqrFt']=$sqrFt;
+            $member = $_SESSION['member'];
+            $member->setLocation($location);
+            $member->setSqrFt($sqrFt);
+            $_SESSION['member'] = $member;
+            header("Location:style");
+        }
+    }
+    $f3->set('location',$location);
+    $f3->set('sqrFt',$sqrFt);
+    $f3->set('errors',$errors);
+
+
     $template = new Template();
-    echo $template->render('pages/information.html');
+    echo $template->render('pages/estimate.html');
 });
+
+$f3 -> route('GET|POST /style', function() {
+    $template = new Template();
+    echo $template->render('pages/style.html');
+});
+
+
+
 
 
 
